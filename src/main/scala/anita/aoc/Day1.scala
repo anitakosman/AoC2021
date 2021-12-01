1282c1a1ac51f255416b52e18f959f919b429b57
Anita Kosman <anitakosman@hotmail.com>
2021-12-01 12:12:56 +0100

    Cleaned up code day 1

--- src/main/scala/anita/aoc/Day1.scala
+++ src/main/scala/anita/aoc/Day1.scala
@@ -7,22 +7,22 @@ object Day1 {
   def part1(): Int = {
     val resource = Source.fromResource("day1.txt")
     val depths = resource.getLines().toList.map(_.toInt)
-    val differences = zipWithNext(depths).map(d => d._2 - d._1)
     resource.close()
-    differences.count(d => d > 0)
+    countIncreases(depths)
   }
 
   def part2(): Int = {
     val resource = Source.fromResource("day1.txt")
     val depths = resource.getLines().toList.map(_.toInt)
-    val triples = zipWithNextTwo(depths).map(a => a._1 + a._2 + a._3)
-    val differences = zipWithNext(triples).map(d => d._2 - d._1)
     resource.close()
-    differences.count(d => d > 0)
+    val triples = depths.sliding(3).toList.map(_.sum)
+    countIncreases(triples)
   }
 
   def main(args: Array[String]): Unit ={
     println(part1())
     println(part2())
   }
+
+  def countIncreases(l: List[Int]): Int = zipWithNext(l).map { case (a, b) => b - a }.count(d => d > 0)
 }
--- src/main/scala/anita/aoc/Util.scala
+++ src/main/scala/anita/aoc/Util.scala
@@ -1,13 +1,5 @@
 package anita.aoc
 
 object Util {
-  def zipWithNext[A](depths: List[A]): List[(A,A)] = depths match {
-    case a :: b :: tail => (a, b) :: zipWithNext(b :: tail)
-    case _ => Nil
-  }
-
-  def zipWithNextTwo[A](depths: List[A]): List[(A,A,A)] = depths match {
-    case a :: b :: c :: tail => (a, b, c) :: zipWithNextTwo(b :: c :: tail)
-    case _ => Nil
-  }
+  def zipWithNext[A](l: List[A]): List[(A,A)] = l zip l.tail
 }
