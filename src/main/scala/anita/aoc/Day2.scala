742a614c560cf6d293a3477212a2276c687764f4
Anita Kosman <anitakosman@hotmail.com>
2021-12-02 06:55:38 +0100

    Finished Day2

---
+++ src/main/scala/anita/aoc/Day2.scala
@@ -0,0 +1,48 @@
+package anita.aoc
+
+import anita.aoc.Util._
+
+import scala.annotation.tailrec
+import scala.io.Source
+import scala.util.matching.Regex
+
+object Day2 {
+
+  def part1(): Int = {
+    val resource = Source.fromResource("day2.txt")
+    val input = resource.getLines().toList
+    resource.close()
+    val (x,d) = followMadeUpInstructions(input, 0, 0)
+    x * d
+  }
+
+  def part2(): Int = {
+    val resource = Source.fromResource("day2.txt")
+    val input = resource.getLines().toList
+    resource.close()
+    val (x,d) = followActualInstructions(input, 0, 0, 0)
+    x * d
+  }
+
+  val instruction = new Regex("(forward|down|up) (\\d+)")
+  @tailrec
+  def followMadeUpInstructions(instructions: List[String], x: Int, d: Int): (Int, Int) = instructions match {
+    case instruction("forward", n) :: tail => followMadeUpInstructions(tail, x + n.toInt, d)
+    case instruction("down", n) :: tail => followMadeUpInstructions(tail, x, d + n.toInt)
+    case instruction("up", n) :: tail => followMadeUpInstructions(tail, x, d - n.toInt)
+    case Nil => (x, d)
+  }
+
+  @tailrec
+  def followActualInstructions(instructions: List[String], x: Int, d: Int, a: Int): (Int, Int) = instructions match {
+    case instruction("forward", n) :: tail => followActualInstructions(tail, x + n.toInt, d + (n.toInt * a), a)
+    case instruction("down", n) :: tail => followActualInstructions(tail, x, d, a + n.toInt)
+    case instruction("up", n) :: tail => followActualInstructions(tail, x, d, a - n.toInt)
+    case Nil => (x, d)
+  }
+
+  def main(args: Array[String]): Unit ={
+    println(part1())
+    println(part2())
+  }
+}
