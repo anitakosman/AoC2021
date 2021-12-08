d9153f0578a76fed49a368790b85b11559fb22c4
Anita Kosman <anitakosman@hotmail.com>
2021-12-08 07:57:16 +0100

    Refactored Day7

--- src/main/scala/anita/aoc/Day7.scala
+++ src/main/scala/anita/aoc/Day7.scala
@@ -5,22 +5,21 @@ import anita.aoc.Util._
 import scala.annotation.tailrec
 
 object Day7 {
-  def part1(crabs: Array[Int], min: Int, max: Int): Int = {
-    (min to max).map(x => crabs.map(c => Math.abs(c - x)).sum).minBy(identity)
+  def part1(crabs: Array[Int], n: Int): Int = {
+    val median = if (n % 2 == 1) crabs.drop(n / 2).head else (crabs.drop((n / 2) - 1).head + crabs.drop(n / 2).head) / 2
+    crabs.map(c => Math.abs(c - median)).sum
   }
 
-  def part2(crabs: Array[Int], min: Int, max: Int): Int = {
-    (min to max).map(x => crabs.map(c => triangularNumber(Math.abs(c - x))).sum).minBy(identity)
+  def part2(crabs: Array[Int], n: Int): Int = {
+    val average = crabs.sum / n
+    crabs.map(c => triangularNumber(Math.abs(c - average))).sum
   }
 
   def main(args: Array[String]): Unit ={
     val crabs = parseInput()
     val n = crabs.length
-    val average = crabs.sum / n
-    val median = if (n % 2 == 1) crabs.drop(n / 2).head else (crabs.drop((n / 2) - 1).head + crabs.drop(n / 2).head) / 2
-    val (a,b) = if (median <= average) (median, average) else (average, median)
-    println(part1(crabs, a, b))
-    println(part2(crabs, a, b))
+    println(part1(crabs, n))
+    println(part2(crabs, n))
   }
 
   def parseInput():  Array[Int] = {
