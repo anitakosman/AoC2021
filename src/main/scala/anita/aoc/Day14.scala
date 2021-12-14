087a9fd86cc57772850cf1d969123c0d59b28acd
Anita Kosman <anitakosman@hotmail.com>
2021-12-14 08:34:43 +0100

    Finished Day14

--- src/main/scala/anita/aoc/Day14.scala
+++ src/main/scala/anita/aoc/Day14.scala
@@ -7,35 +7,38 @@ import scala.collection.immutable.Map.WithDefault
 import scala.util.matching.Regex
 
 object Day14 {
-  def part1(template: String, rules: Map[String, String]): Int = {
-    val counts = insert(template, rules, 10).groupBy(identity).map{ case (k, v) => v.length }
+  def solve(template: Map[String, Long], rules: Map[String, List[String]], start: Char, end: Char, steps: Int): Long = {
+    val counts = insert(template, rules, steps).toList
+      .flatMap{ case (s, count) => List((s.charAt(0), count), (s.charAt(1), count)) }
+      .groupBy(_._1)
+      .map{
+        case (k, v) if k == start || k == end => (v.map(_._2).sum + 1) / 2
+        case (_, v) => v.map(_._2).sum / 2
+      }
     counts.max - counts.min
   }
 
-  def part2(template: String, rules: Map[String, String]): Int = {
-    ???
-  }
-
   def main(args: Array[String]): Unit = {
-    val (template, rules) = parseInput()
-    println(part1(template, rules))
-    println(part2(template, rules))
+    val (template, rules, start, end) = parseInput()
+    println(solve(template, rules, start, end, 10))
+    println(solve(template, rules, start, end, 40))
   }
 
-  def parseInput(): (String, WithDefault[String, String]) = {
+  def parseInput(): (Map[String, Long], Map[String, List[String]], Char, Char) = {
     val input = getInput(14)
 
-    val template = input.head
+    val template = input.head.sliding(2).toList.groupBy(identity).map{ case (k, v) => (k, v.size.toLong) }
     val ruleRegex = new Regex("([A-Z])([A-Z]) -> ([A-Z])")
-    val rules = input.drop(2).map{ case ruleRegex(a, b, c) => (s"$a$b", s"$a$c$b") }.toMap
-    (template, new WithDefault(rules, identity[String]))
+    val rules = input.drop(2).map{ case ruleRegex(a, b, c) => (s"$a$b", List(s"$a$c", s"$c$b")) }.toMap
+    (template, new WithDefault(rules, List(_)), input.head.charAt(0), input.head.charAt(input.head.length-1))
   }
 
   @tailrec
-  def insert(template: String, rules: Map[String, String], n: Int): String = {
+  def insert(template: Map[String, Long], rules: Map[String, List[String]], n: Int): Map[String, Long] = {
     if (n <= 0) template
     else {
-      val inserted = template.sliding(2).map(rules(_).dropRight(1)).mkString + template.takeRight(1)
+      val inserted = template.toList.flatMap { case (s, count) => rules(s).map((_, count)) }
+        .groupBy(_._1).map{ case(s, counts) => (s, counts.map(_._2).sum) }
       insert(inserted, rules, n - 1)
     }
   }
